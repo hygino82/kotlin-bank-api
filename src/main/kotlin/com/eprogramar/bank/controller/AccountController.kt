70f43bb8bffc7372002b051d16299fa7c7690b36
Adroaldo <hygino@msn.com>
2024-11-24 11:17:40 -0300

    Update, delete

--- src/main/kotlin/com/eprogramar/bank/controller/AccountController.kt
+++ src/main/kotlin/com/eprogramar/bank/controller/AccountController.kt
@@ -2,12 +2,16 @@ package com.eprogramar.bank.controller
 
 import com.eprogramar.bank.model.Account
 import com.eprogramar.bank.repository.AccountRepository
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -26,4 +30,21 @@ class AccountController(val repository: AccountRepository) {
             ResponseEntity.ok(it)
         }.orElse(ResponseEntity.notFound().build())
 
+    @PutMapping("/{id}")
+    fun update(@PathVariable id: Long, @RequestBody account: Account): ResponseEntity<Account> =
+        repository.findById(id).map {
+            val accountToUpdate = it.copy(
+                name = account.name,
+                phone = account.phone,
+                document = account.document
+            )
+            ResponseEntity.ok(repository.save(accountToUpdate))
+        }.orElse(ResponseEntity.notFound().build())
+
+    @DeleteMapping("/{id}")
+    fun deleteById(@PathVariable id: Long): ResponseEntity<Void> =
+        repository.findById(id).map {
+            repository.delete(it)
+            ResponseEntity<Void>(HttpStatus.OK)
+        }.orElse(ResponseEntity.notFound().build())
 }
